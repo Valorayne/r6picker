7d54ebacf1e27a5f1b95e41191015fcb7ec446e0
Valorayne <kanu.privat@gmail.com>
2023-03-23 16:25:09 +0100

    Set up first hapi server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Refresh again!")
+import Hapi from "@hapi/hapi"
+
+async function runServer() {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+    await server.start();
+    return server
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+runServer().then(server => console.log('Server running on %s', server.info.uri))
