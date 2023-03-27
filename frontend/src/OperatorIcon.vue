7c393bc0c60ee23010bca5a19a600d7c89e411e7
Valorayne <kanu.privat@gmail.com>
2023-03-27 09:16:32 +0200

    Adding Tailwind CSS

--- frontend/src/OperatorIcon.vue
+++ frontend/src/OperatorIcon.vue
@@ -4,10 +4,12 @@ export type OperatorIconProps = {
   attributes: Record<string, unknown>
   contents: string
   size?: number
+  selected?: boolean
 }
-withDefaults(defineProps<OperatorIconProps>(), { size: 50 })
+withDefaults(defineProps<OperatorIconProps>(), { size: 50, selected: false })
 </script>
 
 <template>
-  <svg v-bind="attributes" v-html="contents" :width="size" :height="size"/>
+  <svg v-bind="attributes" v-html="contents" :width="size" :height="size"
+       class="hover:cursor-pointer"/>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, Plugin } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from "vue-query"
 
 import App from './App.vue'
+import '../index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(VueQueryPlugin)
+app.use(VueQueryPlugin as Plugin)
 app.mount('#app')
